2986257a28d8ab185df3708d20a0aa6af1e32ae9
Rickenbacker620 <fu78sion@gmail.com>
2025-11-02 14:37:13 -0500

    fix zoxide

--- dot_config/fish/conf.d/integrations.fish
+++ dot_config/fish/conf.d/integrations.fish
@@ -1,5 +1,5 @@
 # Third-party tool integrations
 if status --is-interactive
-    zoxide init fish | source
+    ~/.local/share/mise/shims/zoxide init fish | source
     ~/.local/bin/mise activate fish | source
 end

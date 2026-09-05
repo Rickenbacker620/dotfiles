67768bba93c733ab7ebb76b16b7b137d152f7691
Rickenbacker620 <fu78sion@gmail.com>
2026-09-05 16:42:57 -0400

    remove arch desktop

--- dot_config/fish/conf.d/hyprland.fish
+++
@@ -1,6 +0,0 @@
-# is-interactive, not is-login: the .bashrc handoff is a bare `exec fish`.
-if status is-interactive; and test (tty) = "/dev/tty1"
-    if type -q Hyprland
-        exec Hyprland
-    end
-end
